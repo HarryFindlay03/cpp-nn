3ae26dbd0c514035963874b9b2edd48c531e2621
Harry Findlay <harryfindlay@outlook.com>
2024-03-25 16:12:54 +0000

    removing not needed

--- include/ML_ANN.h
+++ include/ML_ANN.h
@@ -16,24 +16,11 @@ public:
 
     static Eigen::MatrixXd elem_wise_product(const Eigen::MatrixXd& lhs, const Eigen::MatrixXd& rhs);
 
-    Eigen::MatrixXd forward_propogate(const Eigen::MatrixXd& data);
     double forward_propogate_rl(const std::vector<double>& data);
 
-    void back_propogate(const Eigen::MatrixXd& yhat, const Eigen::MatrixXd& labels);
     void back_propogate_rl(const double output, const double target);
 
-    void update_weights(size_t learning_rate);
     void update_weights_rl(const double eta);
-
-    void evaluate(
-        const Eigen::MatrixXd& train_data, 
-        const Eigen::MatrixXd& train_label, 
-        const Eigen::MatrixXd& test_data, 
-        const Eigen::MatrixXd& test_labels,
-        size_t num_epochs,
-        double eta,
-        bool eval_train,
-        bool eval_test);
 };
 
 #endif
